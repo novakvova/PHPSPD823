f222aa91913527ffe019aee5c4869a5b281f82a9
novakvova <novakvova@gmail.com>
2020-05-30 14:41:38 +0300

    add dcker-cmpose

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_NAME','dbuser');
-define('DB_USER','admin_db');
-define('DB_PASSWORD','123456');
-define('DB_HOST','localhost');
+define('DB_NAME','shop');
+define('DB_USER','plomba');
+define('DB_PASSWORD','Olke632id&dje*7dje6hdne7&&dGGtde');
+define('DB_HOST','mysql');
